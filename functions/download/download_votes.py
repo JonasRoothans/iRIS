6cdc743e88b472f3ad22c608e10873fb14246973
Jonas <jroothans@gmail.com>
2026-05-13 15:32:40 +0200

    cleaning of Votes

--- functions/download/download_votes.py
+++ functions/download/download_votes.py
@@ -85,13 +85,6 @@ def get_module_from_meeting_url(meeting_url,vote_title,vote_id):
 
 
 def update_vote_per_member(driver, url,fromDate):
-
-
-    # Initialize counters
-    total_votes_count = 0
-    corresponding_votes_count = 0
-    other_count = 0
-
     print(url)
     try:
         soup = web.visitPageWithDriver(driver,url)
@@ -103,64 +96,48 @@ def update_vote_per_member(driver, url,fromDate):
         print(f'could not load {url}')
         return
 
-    # Find the script tag containing the JSON data
-    script_tag = soup.find('script', id='vote_data')
-
     # Find the ID
-    member_id = url.split('/lid/')[1].split('/')[0]#soup.find('h2', class_='lid_header')['data-role_id']
+    member_id = url.split('/lid/')[1].split('/')[0]
+
 
     # Extract the JSON data from the script tag
+    script_tag = soup.find('script', id='vote_data')
     if script_tag:
         json_data = script_tag.string
-
-        # Parse the JSON data
         data = json.loads(json_data)
 
-        # Access the specific data you need
-        total_votes = data['total_votes']
-        # Access the recent votes
-        recent_votes = data['recent_votes']
-
         # Iterate over recent votes
+        recent_votes = data['recent_votes']
         if recent_votes:
             for vote_id, vote_data in recent_votes.items():
 
                 #check if vote is within timeframe:
                 date = datetime.strptime(vote_data['date']['date'][0:10],'%Y-%m-%d')
-                #print(vote_data['date']['date'][0:10])
                 if date < fromDate:
-                 #   print('vote out of scope')
-                    continue
-
-
-
-                total_votes_count += 1
-                vote = vote_data['vote']
-                result = vote_data['result']
-
-                # Check if vote is 'voor' and result is 'aangenomen'
-                if vote == 'voor' and result == 'aangenomen':
-                    corresponding_votes_count += 1
-                elif vote == 'tegen' and result == 'verworpen':
-                    corresponding_votes_count += 1
-                else:
-                    other_count += 1
-
-
+                    break
 
                 # Save this information to the Vote Object
                 v = Vote(int(vote_id))
+                if v.description is None:
+                    v.description = vote_data['title']
+                if v.result is None:
+                    v.result = vote_data['result']
+                if v.url is None:
+                    v.url = vote_data['url']
+                if v.date is None:
+                    v.date = vote_data['date']['date'][0:10]
+                v.add_membervote(int(member_id), vote_data['vote'])
+
+
+                # pair with module module
                 if v.module_id is None:
-                    #print('ALL VOTES ARE REASSIGNED. REMOVE THIS WHEN DONE')
-                    # -- get module if it is still empty (NEW vote)
                     try:
                         m = get_module_from_meeting_url(vote_data['url'], vote_data['title'], int(vote_id))
                     except Exception as e:
                         print(f"Error loading module {vote_data['url']}:{e}")
                         m = Module()
                     v.module_id = m.module_id
-                elif isinstance(v.module_id, str) and len(v.module_id)<1 and  v.module_id[1]=='_':
-                    # -- get module if it could not find it previously.
+                elif isinstance(v.module_id, str) and len(v.module_id) < 1 and v.module_id[1] == '_':
                     try:
                         m = get_module_from_meeting_url(vote_data['url'], vote_data['title'], int(vote_id))
                     except Exception as e:
@@ -169,15 +146,6 @@ def update_vote_per_member(driver, url,fromDate):
                     v.module_id = m.module_id
                 else:
                     m = Module(v.module_id)
-                if v.description is None:
-                    v.description = vote_data['title']
-                if v.result is None:
-                    v.result = vote_data['result']
-
-                if v.url is None:
-                    v.url = vote_data['url']
-                if v.date is None:
-                    v.date = vote_data['date']['date'][0:10]
 
                 #sync data.
                 m.date = v.date
@@ -187,46 +155,19 @@ def update_vote_per_member(driver, url,fromDate):
                 m.vote_id = v.vote_id
                 m.connectToMeeting()
                 m.save()
-                v.add_membervote(int(member_id), vote)
-
-
-                #------DEBUG
-                #if total_votes_count == 20:
-                 #   print('DEBUG MODE: CONTINUE AFTER 20 VOTES')
-                 #   break
-                #-----
-
-
-            # Calculate the percentage of corresponding votes
-            percentage_corresponding_votes = (
-                                                         corresponding_votes_count / total_votes_count) * 100 if total_votes_count > 0 else 0
-        else:
-            percentage_corresponding_votes = 0
-
-        return percentage_corresponding_votes, member_id
+        return
 
 
 
 def download_votes(driver,fromDate):
-
-
-
     #--get all members
     folder_path = cwdpath(os.path.join('json','members','speaker'))
     member_ids = os.listdir(folder_path)
+
+    #loop over all files
     for member_id in member_ids:
         member = Member(member_id)
         print(member.name)
         if member.url:
             update_vote_per_member(driver, member.url,fromDate)
-
-
-        # DEBUG----
-        #print('DEBUG MODE WILL CONTINUE AFTER FIRST MEMBER')
-        #return
-        #-----
-
-
-
-    #-- loop over members
 
